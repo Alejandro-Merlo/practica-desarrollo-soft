dad0f9c12c8ee6019fa75962e26fec2965dc7f97
Alejandro <merloalejandro@gmail.com>
2015-07-10 19:54:07 -0300

    Corrijo las tasks y elimino archivos de mas

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,5 +38,5 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-wiredep');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.registerTask('default', 'jshint');
+  grunt.registerTask('default', ['jshint', 'wiredep']);
 };
